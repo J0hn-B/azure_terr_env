03dd7143062a46de596390a82db22d003027521a
J0hn-B <connect-wsman@hotmail.com>
2021-02-04 11:22:32 +0000

    update folders

---
+++ azuread/terr_init.tf
@@ -0,0 +1,14 @@
+provider "azurerm" {
+  features {}
+}
+provider "azuread" {}
+
+#* Backend for remote state. Create Storage first.
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraform-tfstate"
+    storage_account_name = "terrstatedev"
+    container_name       = "terraformdev"
+    key                  = "azure_ad.tfstate"
+  }
+}
