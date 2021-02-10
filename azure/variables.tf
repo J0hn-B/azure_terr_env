7d9dc681601404514c8617a40b3501dad4f9da35
J0hn-B <connect-wsman@hotmail.com>
2021-02-10 19:36:32 +0000

    update vm name

--- azure/variables.tf
+++ azure/variables.tf
@@ -7,11 +7,11 @@ variable "rg_location" { default = "North Europe" }
 variable "aks_node_rg_name" { default = "ltd-aks-resources" }
 
 # Azure Linux Virtual Machines Resource Group
-variable "rg_linux_vm_name" { default = "ltd-vm-resources" }
+variable "rg_linux_vm_name" { default = "ltd-linux-vm-resources" }
 variable "rg_linux_vm_location" { default = "North Europe" }
 
 # Azure Linux Virtual Machines 
-variable "linux_vm_01_name" { default = "lnx-vm-01" }
+variable "linux_vm_01_name" { default = "linux-vm-01" }
 
 # Azure Virtual Network Resources
 variable "rg_vnet_resources_name" { default = "ltd-virtual-networks-resources" }
